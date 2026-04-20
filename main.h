e413aae4f077847f894c790d9e7a33501b246b79
WhyNotJava <>
2026-04-20 12:34:04 -0400

    added libs for errno

--- main.h
+++ main.h
@@ -5,7 +5,9 @@
 #include <sodium.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <sys/socket.h>
