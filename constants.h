1a5e683c1b534b6d0c9143e5d71842758b98ad2e
WhyNotJava <>
2026-04-26 20:58:33 -0400

    add new constants

--- constants.h
+++ constants.h
@@ -7,6 +7,9 @@
     #define INDENT_CHAR ' '
     #define PADDING_MIN 0 
     #define PADDING_MAX 100
+    #define SEND_CERTIFICATE 1
+    #define KEY_FILE "keys/ec_key.hex"
+    #define CERT_FILE "keys/cert.der"
 
     #define SECRET_LENGTH 32
     #define AEAD_TAG_LENGTH 16
