34beede30bb0e8a7b3771300a4612c6c0b6bb6ca
WhyNotJava <>
2026-05-15 22:37:17 -0400

    MACROS ARE NOT FUNCTIONS. MACROS ARE NOT FUNCTIONS. MACROS ARE NOT FUNCTIONS.

---
+++ alert_constants.c
@@ -0,0 +1,61 @@
+#include "main.h"
+char* get_alert_name(int type){
+        switch(type){
+            case 0:
+                    return "close_notify";
+            case 10:
+                    return "unexpected_message";
+            case 20:
+                    return "bad_record_mac";
+            case 22:
+                    return "record_overflow";
+            case 40:
+                    return "handshake_failure";
+            case 42:
+                    return "bad_certificate";
+            case 43:
+                    return "unsupported_certificate";
+            case 44:
+                    return "certificate_revoked";
+            case 45:
+                    return "certificate_expired";
+            case 46:
+                    return "certificate_unknown";
+            case 47:
+                    return "illegal_parameter";
+            case 48:
+                    return "unknown_ca";
+            case 49:
+                    return "access_denied";
+            case 50:
+                    return "decode_error";
+            case 51:
+                    return "decrypt_error";
+            case 70:
+                    return "protocol_version";
+            case 71:
+                    return "insufficient_security";
+            case 80:
+                    return "internal_error";
+            case 86:
+                    return "inappropriate_fallback";
+            case 90:
+                    return "user_canceled";
+            case 109:
+                    return "missing_extension";
+            case 110:
+                    return "unsupported_extension";
+            case 112:
+                    return "unrecognized_name";
+            case 113:
+                    return "bad_certificate_status_response";
+            case 115:
+                    return "unknown_psk_identity";
+            case 116:
+                    return "certificate_required";
+            case 120:
+                    return "no_application_protocol";
+            default:
+                    return "unknown";
+        }
+    }
